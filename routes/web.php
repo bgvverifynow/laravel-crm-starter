debd1c20ab9a1740052950ab28de16903106a873
bgvverifynow <bgv.verifynow@gmail.com>
2026-05-16 16:09:35 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,35 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+/*
+|--------------------------------------------------------------------------
+| Create Admin User
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/create-admin-verify', function () {
+
+    $user = \App\Models\User::updateOrCreate(
+        ['email' => 'admin@verifynow.in'],
+        [
+            'name' => 'Admin',
+            'password' => \Illuminate\Support\Facades\Hash::make('Admin@123'),
+        ]
+    );
+
+    return response()->json([
+        'success' => true,
+        'message' => 'Admin Created'
+    ]);
+
+});
+
+/*
+|--------------------------------------------------------------------------
+| Make Super Admin
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/make-super-admin', function () {
 
     $user = \App\Models\User::where('email', 'admin@verifynow.in')->first();
@@ -31,6 +60,30 @@ Route::get('/make-super-admin', function () {
     $user->assignRole($role);
 
     return 'Super Admin Assigned';
+
+});
+
+/*
+|--------------------------------------------------------------------------
+| Fix Permissions
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/fix-permissions', function () {
+
+    $user = \App\Models\User::where('email', 'admin@verifynow.in')->first();
+
+    if (!$user) {
+        return 'User not found';
+    }
+
+    $permissions = \Spatie\Permission\Models\Permission::all();
+
+    foreach ($permissions as $permission) {
+        $user->givePermissionTo($permission);
+    }
+
+    return 'Permissions Fixed';
 
 });
 
